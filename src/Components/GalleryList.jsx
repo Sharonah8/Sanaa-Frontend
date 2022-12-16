f7f8b0aeca7a2b9640ffd5701e290c224855b33b
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-12-16 17:39:28 +0300

    add file || create reusable cards

---
+++ src/Components/GalleryList.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Gallery from './Gallery'
+function GalleryList({images}) {
+ 
+  return (
+    <div>
+        {images.map((img)=>{
+          return(
+            <Gallery key={img} img={img} />
+          );
+        })
+        }
+   
+    </div>
+  )
+}
+
+export default GalleryList
