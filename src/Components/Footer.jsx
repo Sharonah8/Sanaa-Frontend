cea820efa8bd5cb1049eb437ffb37d4b1a8cf004
Sharonah8 <nyamongosharon88@gmail.com>
2022-12-17 16:24:24 +0300

    Social media icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <h2>The journey is the reward</h2>
+      <p>Live Intentionally . Live Authentically</p>
+      <SocialIcon url="https://twitter.com/jaketrent" />
+      <SocialIcon url="https://linkedin.com/in/jaketrent" />
+      <SocialIcon url="https://facebook.com/in/jaketrent" />
+      <SocialIcon url="https://instagram.com/in/jaketrent" />
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
