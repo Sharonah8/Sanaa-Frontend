3290195cdef4589ba5342bfb708ce3a5f9439cfe
Sharonah8 <nyamongosharon88@gmail.com>
2022-12-17 21:13:12 +0300

    About us content

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React from "react";
 import "../Styles/About.css";
 
 function About() {
   return (
     <div>
-      <h1>About Us</h1>
-      <p>Sanaa is an e-commerce platform designed to host classic art peices. </p>
-
+      <h1 className="about-heading">About Us</h1>
+      <p className="about-paragraph">
+        Sanaa is an e-commerce platform designed to host classic art peices. It
+        seeks to bridge the gap between local and international artists and
+        their customers. It provides a platform for them to interact and buy and
+        sell their art pieces without the need for a middle-man, thus promoting
+        the artists and providing a safe platform for transaction between the
+        artists and the buyers.
+      </p>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
