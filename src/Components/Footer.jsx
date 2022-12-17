f347104b3aedc54fb38352dae7def6350acc0334
Sharonah8 <nyamongosharon88@gmail.com>
2022-12-17 18:42:44 +0300

    Space social media icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import "../Styles/Footer.css";
 
 function Footer() {
   return (
     <footer>
       <h2>The journey is the reward</h2>
       <p>Live Intentionally . Live Authentically</p>
-      <SocialIcon url="https://twitter.com/jaketrent" />
-      <SocialIcon url="https://linkedin.com/in/jaketrent" />
-      <SocialIcon url="https://facebook.com/in/jaketrent" />
-      <SocialIcon url="https://instagram.com/in/jaketrent" />
+      <SocialIcon url="https://twitter.com/jaketrent" style={{ margin: 10 }} />
+      <SocialIcon
+        url="https://linkedin.com/in/jaketrent"
+        style={{ margin: 10 }}
+      />
+      <SocialIcon
+        url="https://facebook.com/in/jaketrent"
+        style={{ margin: 10 }}
+      />
+      <SocialIcon
+        url="https://instagram.com/in/jaketrent"
+        style={{ margin: 10 }}
+      />
     </footer>
   );
 }
