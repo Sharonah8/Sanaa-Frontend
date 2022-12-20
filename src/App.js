b6adb2ce46f944bb451295a4a9c8abf09e977bdf
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-12-20 06:54:36 +0300

    adds login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 
 import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LogIn from './Components/LogIn';
+import SignUp from './Components/SignUp'
 
 function App() {
   return (
     <div className="App">
       <h1>Sanaa</h1>
+      {/* <Routes>
+        <Route exact path="/" element={<LogIn />} />
+        <Route path="/Login" element={<SignUp />} />
+      </Routes> */}
     </div>
   );
 }
--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -1,9 +1,72 @@
 import React from 'react'
+import {useState, useNavigate} from 'react'
 
 function LogIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(email, password);
+    
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    }).then((r) => {
+      
+      if (r.ok) {
+        r.json().then(() => {
+          navigate("/Gallery")
+
+        });
+      } else {
+        r.json().then((err) => console.log(err));
+      }
+    });
+  };
+
   return (
-    <div>LogIn</div>
-  )
-}
+    <div className="form">
+      <form onSubmit={handleSubmit}>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="username"
+            autoComplete="off"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="nome@email.com.br"
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            autoComplete="current-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+          />
+        </div>
+        <button
+          type="submit"
+          className="primary"
+          onClick={() => handleSubmit()}
+        >
+          
+        </button>
+      </form>
+    </div>
+  );
+};
+
 
 export default LogIn
