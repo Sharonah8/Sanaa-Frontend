6acbce7ad9577cc8bd7e6f765ca641978ff950b1
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-12-23 21:54:59 +0300

    Change image url to link instead of relative path

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -13,11 +13,11 @@ function HomePage() {
           <div className='centered-text'>
             <h2>Sanaa <br></br> Art</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-            <button>Go to store</button>
+            <button id="home-page-btn">Go to store</button>
           </div>
           <div classname='overlay'>
-            <img src={SecondImage} height="350px" alt="image" />
-            <p>round image</p>
+            <img className='home-page-img' src="https://i.ibb.co/1nwd6B3/909b68afdf7ca5550fa298dc06657ab0.jpg"  alt="image" />
+            {/* <p>round image</p> */}
           </div>
         </div>
       </div>
