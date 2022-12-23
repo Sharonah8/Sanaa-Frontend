775f3760bb08b74acb1e944ddd31bf2d933b5927
mulukimwangeka <mwangekaruth@gmail.com>
2022-12-23 11:00:26 +0300

    Navbar working successfully

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-
+import React from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar';
+import HomePage from './Components/HomePage';
+import About from './Components/About';
+import Gallery from './Components/Gallery';
+import LogIn from './Components/LogIn';
+import Signup from './Components/SignUp';
+import AddGallery from './Components/AddGallery';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Sanaa</h1>
+      <NavBar />
+      <Routes>
+        
+        <Route exact path="/" element={<HomePage/>} />
+        <Route exact path="/about" element={<About/>} />
+        <Route exact path="/gallery" element={<Gallery/>} />
+        <Route exact path="/login" element={<LogIn/>} />
+        <Route exact path="/signup" element={<Signup/>} />
+        <Route exact path="/addgallery" element={<AddGallery/>} />
+      </Routes>
+
     </div>
   );
 }
+
 
 export default App;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+//import navbar.css from styles
+import '../Styles/NavBar.css';
+import { Link } from 'react-router-dom';
+
+
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <h1 class="navbar-brand text-light" href="#">Sanaa</h1>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-between" id="navbarText">
+      <div className="d-flex">
+         
+      </div>
+      <div className="d-flex">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+          <Link class="nav-link text-light" to="/">HomePage</Link>
+          </li>
+          <li class="nav-item">
+          <Link class="nav-link text-light" to="/About">About</Link>
+          </li>
+          <li class="nav-item">
+          <Link class="nav-link text-light" to="/Gallery">Gallery</Link>
+          </li>
+          <li class="nav-item">
+          <Link class="nav-link text-light" to="/AddGallery">AddGallery</Link>
+          </li>
+          <li class="nav-item">
+          <Link class="nav-link text-light" to="/LogIn">LogIn</Link>
+          </li>
+          <li class="nav-item">
+          <Link class="nav-link text-light" to="/SignUp">SignUp</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
   )
+    
 }
+
+
+
 
 export default NavBar
