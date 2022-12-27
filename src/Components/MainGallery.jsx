89968d3fa2c2463bf7843846f49e1283ad6d8a64
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-12-27 13:45:34 +0300

    added search to render on page

--- src/Components/MainGallery.jsx
+++ src/Components/MainGallery.jsx
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from "react";
+import Search from "./Search";
 import GalleryList from "./GalleryList"
 
 
 function MainGallery() {
   const [images, setImages] = useState([]);
+  const [searchValue, setSearchValue] = useState([]);
   useEffect(() => {
     fetch("http://localhost:3000/images")
       .then((res) => res.json())
       .then((resp) => {
         setImages(resp);
+        setSearchValue(resp);
        
       });
   }, []);
 
+  function change(event) {
+    setImages(
+      searchValue.filter((images) =>
+        images.title.toLowerCase().includes(event.target.value.toLowerCase())
+      )
+    );
+  }
+
   return (
     
     <div className="main-gallery">
+      <Search change={change} />
       <GalleryList images = {images}/>
     </div>
   )
