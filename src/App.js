716b91de62e1a87c7295ac6d5d6fa8ea048e87b9
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-12-27 14:22:12 +0300

    checking file for update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import NavBar from './Components/NavBar';
+import HomePage from './Components/HomePage';
+import About from './Components/About';
 import MainGallery from './Components/MainGallery';
+import AddGallery from './Components/AddGallery';
+import Footer from './Components/Footer';
+import LogIn from './Components/LogIn';
+import Signup from './Components/SignUp';
+
+
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import LogIn from './Components/LogIn';
+import SignUp from './Components/SignUp';
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import LogIn from './Components/LogIn';
+// import SignUp from './Components/SignUp'
 
 function App() {
   return (
     <div className="App">
-      <MainGallery />
+<<<<<<< HEAD
+      <NavBar />
+
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/gallery" element={<MainGallery/>} />
+        <Route exact path="/addgallery" element={<AddGallery />} />
+        <Route exact path="/login" element={<LogIn />} />
+        <Route exact path="/signup" element={<Signup />} />
+      </Routes>
+      
+      
+=======
+      <h1>Sanaa</h1>
+      <SignUp />
+      {/* <LogIn /> */}
+      {/* <Routes>
+        <Route exact path="/" element={<LogIn />} />
+        <Route path="/Login" element={<SignUp />} />
+      </Routes> */}
+>>>>>>> ft-login
     </div>
   );
 }
+
 
 export default App;
