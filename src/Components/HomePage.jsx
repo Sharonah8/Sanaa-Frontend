36d0cf6469f5a3551dd1fee3aed2ab5cf8634b68
Gacheruian <ian.gacheru@student.moringaschool.com>
2023-01-10 14:45:08 +0300

    making updates to the branches

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "../Styles/HomePage.css";
-import Footer from './Footer';
+import Footer from "./Footer";
+import { Link } from "react-router-dom";
+
 
 function HomePage() {
   return (
@@ -8,13 +10,37 @@ function HomePage() {
       <div className="home-page">
         <div className="home-page-container">
           <div className="centered-text">
-            <h2 className="home-page-heading">
+            {/* <h2 className="home-page-heading">
               Sanaa <br></br> Art
-            </h2>
-            <p className="home-page-paragraph">
-              To understand a work of art, you must develop the ability to perceive. To perceive is to become deeply aware through the senses of the special nature of art.
+            </h2> */}
+            <link
+              href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap"
+              rel="stylesheet"
+            ></link>
+
+            {/* <h1 aria-label="Hi! I'm a developer">
+  Hi! I'm a&nbsp;<span class="typewriter"></span>
+</h1>
+
+<h1 aria-label="Hi! I'm a developer">
+  Hi! I'm a&nbsp;<span class="typewriter thick"></span>
+</h1> */}
+
+            <h1 aria-label="Hi! I'm a developer" id="move-up">
+              Welcome To Sanaa We Offer,
+            </h1>
+            <h1 aria-label="Hi! I'm a developer" id="move-down">
+              &nbsp;
+              <span class="typewriter nocaret"></span>
+            </h1>
+            <p className="home-page-paragraph" id="pofppp">
+              To understand a work of art, you must develop the ability to
+              perceive. To perceive is to become deeply aware through the senses
+              of the special nature of art.
             </p>
+            <Link to={"/Gallery"}>
             <button id="home-page-btn">Go to store</button>
+            </Link>
           </div>
           <div classname="overlay">
             <img
@@ -24,10 +50,8 @@ function HomePage() {
             />
           </div>
         </div>
-       
       </div>
       <Footer />
-
     </main>
   );
 }
